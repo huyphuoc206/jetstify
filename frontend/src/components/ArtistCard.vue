c4500147797c7f7e72f3a7124df881e59d29c865
lnv0507 <lamdilinh.nguyen@gmail.com>
2022-04-13 10:09:25 +0700

    complete interface home

--- frontend/src/components/ArtistCard.vue
+++ frontend/src/components/ArtistCard.vue
@@ -1,20 +1,33 @@
 <template>
   <v-row>
-    <v-col cols="6" sm="3" md="2" v-for="i of 4" :key="i">
+    <v-col cols="12" sm="6" md="4" lg="2" v-for="i of 6" :key="i">
       <v-hover v-slot:default="{ hover }">
-        <v-card class="artist-card" flat>
+        <v-card class="common-card" flat height="300" width="250">
           <v-card-text>
             <v-img
+              height="200"
               class="elevation-10"
-              src="https://direct.rhapsody.com/imageserver/images/alb.261770523/500x500.jpg"
               style="border-radius: 50%"
+              src="https://i.ytimg.com/vi/FN7ALfpGxiI/maxresdefault.jpg"
             >
             </v-img>
+            <div
+              :style="{
+                position: 'absolute',
+                right: '20px',
+                bottom: '85px',
+                cursor: 'auto',
+              }"
+              v-show="hover"
+            >
+              <v-icon color="green" size="60">mdi-play-circle</v-icon>
+            </div>
           </v-card-text>
 
           <v-card-text class="pt-0">
             <p class="body-2 font-weight-medium ma-0 text-truncate white--text">
               <router-link
+                class="white--text text-decoration-none"
                 :to="{
                   name: 'Artist',
                   params: {
@@ -25,13 +38,6 @@
               >
             </p>
             <p class="caption font-weight-light mb-4 text-truncate">Artist</p>
-
-            <div
-              style="position: absolute; right: 10px; bottom: 10px"
-              v-show="hover"
-            >
-              <v-icon size="35">mdi-play-circle-outline</v-icon>
-            </div>
           </v-card-text>
         </v-card>
       </v-hover>
