71cd400044b69b5a20efc27ac0b76a5d86a702e2
lnv0507 <lamdilinh.nguyen@gmail.com>
2022-06-09 21:57:40 +0700

    API upload song

--- backend/src/main/java/com/jestify/service/SongService.java
+++ backend/src/main/java/com/jestify/service/SongService.java
@@ -2,10 +2,13 @@ package com.jestify.service;
 
 import com.jestify.converter.SongConverter;
 import com.jestify.entity.Songs;
+import com.jestify.payload.SongRequest;
 import com.jestify.payload.SongResponse;
 import com.jestify.repository.SongRepository;
+import com.jestify.utils.AmazoneUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
 public class SongService {
     private final SongRepository songRepository;
     private final SongConverter songConverter;
+    private final AmazoneUtil amazoneUtil;
 
     public List<SongResponse> getSongsByArtistId(Long artistID) {
         List<SongResponse> songResponseList = new ArrayList<>();
@@ -23,6 +27,11 @@ public class SongService {
             songResponseList.add(songConverter.toResponse(song));
         }
         return songResponseList;
+    }
+    public SongResponse uploadSongs(SongRequest songRequest, MultipartFile file){
+        songRequest.setLink(amazoneUtil.uploadFile(file));
+        Songs songs = songRepository.save(songConverter.toEntity(songRequest));
+        return songConverter.toResponse(songs);
     }
 
 }
