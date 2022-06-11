40b46a82f00b922a74850f054a9a38304fabff9d
lnv0507 <lamdilinh.nguyen@gmail.com>
2022-06-11 14:41:34 +0700

    done API

--- backend/src/main/java/com/jestify/payload/SongRequest.java
+++ backend/src/main/java/com/jestify/payload/SongRequest.java
@@ -1,13 +1,13 @@
 package com.jestify.payload;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongRequest{
     private String link;
     private String name;
